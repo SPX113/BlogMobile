cb7d45ef2cc2de6e27b8d42e0e6ebb185063e134
SPX113 <1137893057@qq.com>
2021-10-07 09:48:36 +0800

    1

--- mobile/src/network/request.js
+++ mobile/src/network/request.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 export function request(config) {
   const instance = axios.create({
-    baseURL : 'http://localhost:3000/',
+    // baseURL : 'http://localhost:3030/',
+    baseURL : 'http://81.71.19.94:3030/',
     timeout : 5000
   })
   return instance(config)
